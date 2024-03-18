89c94d729cd0cb76a5ae6aaea2b620247e38102f
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-03-18 11:46:20 +0300

    Add project files.

---
+++ First lessons/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace First_lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+
+            string nameUser = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Hello {nameUser}") ;
+        }
+    }
+}
